bc1cf15deaa3bd2602616aaee4fed0de6264ba53
Dafna Libman <dafnalibman@gmail.com>
2020-06-29 00:02:02 +0100

    Add the roll and score functions

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -2,4 +2,12 @@ describe('The Bowling game', function(){
   it('can create a game', function(){
     var game = new BowlingGame();
   }) 
+
+  it('can roll a gutter game', function(){
+    var game = new BowlingGame();
+    for (var i = 0; i < 20; i++){
+      game.roll(0);
+    }
+    expect(game.score()).toBe(0);
+  })
 })
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,3 +1,9 @@
 var BowlingGame = function () {
 
 }
+BowlingGame.prototype.roll = function (pins) {
+
+};
+BowlingGame.prototype.score = function () {
+  return 0
+};
